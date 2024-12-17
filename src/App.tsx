c0447a3bb929d4a13b324752f05c28a5e0e89338
k-kazuma <k_kazuma8@icloud.com>
2024-12-17 13:25:24 +0900

    counter動作確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./counterSlice";
+import { RootState } from "./store";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <>
       <div>{count}</div>
+      <button onClick={() => dispatch(increment())}>++</button>
+      <button onClick={() => dispatch(decrement())}>--</button>
+      <button onClick={() => dispatch(incrementByAmount(count))}>
+        +{count}
+      </button>
     </>
   );
 }
--- src/counterSlice.ts
+++ src/counterSlice.ts
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "./store";
 
 export const counterSlice = createSlice({
   name: "counter",
@@ -12,11 +13,12 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
-    incrementByAmount: (state, action) => {
+    incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
   },
 });
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const selectCount = (state: RootState) => state.counter.value;
 export default counterSlice.reducer;
